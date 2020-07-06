1e8dbe41df5bdfd743e5ff8f130dec34709b52bd
epotapov <43287483+epotapov@users.noreply.github.com>
2020-07-06 00:33:27 -0600

    Working on event handling in FX

--- src/AStar/Main.java
+++ src/AStar/Main.java
@@ -1,18 +1,23 @@
 package AStar;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent.*;
 
 import java.awt.*;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler {
     Scene mainScene;
     Group mainGroup;
     double side = 50;
@@ -31,39 +36,25 @@ public class Main extends Application {
         Canvas canvas = new Canvas(500,800);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         drawShapes(gc);
+        boolean[][] grid = new boolean[18][14];
         mainGroup.getChildren().add(canvas);
         mainScene = new Scene(mainGroup);
         primaryStage.setScene(mainScene);
         primaryStage.show();
     }
 
+    @Override
+    public void handle(Event event) {
+    }
+
     private void drawShapes(GraphicsContext gc) {
-        /*gc.setFill(Color.GREEN);
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-        gc.fillOval(10, 60, 30, 30);
-        gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-        gc.fillPolygon(new double[]{10, 40, 10, 40},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                new double[]{210, 210, 240, 240}, 4);*/
         gc.setFill(Color.GRAY);
         gc.setLineWidth(5);
-        for(double i = 0; i < 50; i += side) {
-            for (double j = 0; j < 100; j += side) {
+        for(double i = 0; i < 900; i += side) {
+            for (double j = 0; j < 700; j += side) {
                 gc.fillRect(i, j, side, side);
             }
         }
     }
+
 }
