3ff2bd2a2586033ebc2f02f87b55b6fa683be792
Edward Potapov <epotapov@comcast.net>
2020-07-08 00:06:13 -0600

    Refined the code

--- src/AStar/Main.java
+++ src/AStar/Main.java
@@ -4,21 +4,17 @@ package AStar;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
 public class Main extends Application {
     Scene mainScene;
     Group mainGroup;
     Pane mainPane;
-    double side = 50;
+    double side = 25;
     cell[][] grid;
     boolean clear;
 
@@ -27,8 +23,6 @@ public class Main extends Application {
     }
 
     public class cell extends Rectangle{
-        boolean red;
-
         public cell(double x, double y, double width, double height) {
             super(x,y,width,height);
         }
@@ -39,15 +33,11 @@ public class Main extends Application {
         primaryStage.setTitle("A* PathFinding");
         primaryStage.setMinHeight(650);
         primaryStage.setMinWidth(1000);
-        Label hello = new Label("what up");
         //mainPane = new Pane();
         mainGroup = new Group();
-        //Canvas canvas = new Canvas(1000,650);
-        //GraphicsContext gc = canvas.getGraphicsContext2D();
-        grid = new cell[20][13];
+        grid = new cell[40][26];
         startGrid();
         clear = false;
-        //mainGroup.getChildren().add(canvas);
         mainScene = new Scene(mainGroup);
         mainGroup.requestFocus();
         mainGroup.setOnMousePressed(e -> {
@@ -77,7 +67,6 @@ public class Main extends Application {
                     break;
             }
         });
-        //primaryStage.setResizable(false);
         primaryStage.setScene(mainScene);
         primaryStage.show();
     }
@@ -94,15 +83,13 @@ public class Main extends Application {
     }
 
     void updateGrid(double x, double y) {
-        for(int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if(x >= grid[i][j].getX() && y >= grid[i][j].getY() && x <= (grid[i][j].getX() + side) && y <= (grid[i][j].getY() + side)) {
-                    if(!clear)
-                        grid[i][j].setFill(Color.BLACK);
-                    else
-                        grid[i][j].setFill(Color.WHITE);
-                }
-            }
-        }
+        int i = (int)(x / side);
+        int j = (int)(y / side);
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length )
+            return;
+        if(!clear)
+            grid[i][j].setFill(Color.BLACK);
+        else
+            grid[i][j].setFill(Color.WHITE);
     }
 }
