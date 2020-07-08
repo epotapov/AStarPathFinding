957102d351194bb992c728e65003d4338e770a68
Edward Potapov <epotapov@comcast.net>
2020-07-07 21:24:29 -0600

    More work on the grid

--- src/AStar/Main.java
+++ src/AStar/Main.java
@@ -19,16 +19,18 @@ public class Main extends Application {
     Group mainGroup;
     Pane mainPane;
     double side = 50;
-    Rectangle[][] grid;
+    cell[][] grid;
+    boolean clear;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public class cell {
+    public class cell extends Rectangle{
         boolean red;
-        public cell() {
 
+        public cell(double x, double y, double width, double height) {
+            super(x,y,width,height);
         }
     }
 
@@ -40,19 +42,40 @@ public class Main extends Application {
         Label hello = new Label("what up");
         //mainPane = new Pane();
         mainGroup = new Group();
-        Canvas canvas = new Canvas(1000,650);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        grid = new Rectangle[20][13];
+        //Canvas canvas = new Canvas(1000,650);
+        //GraphicsContext gc = canvas.getGraphicsContext2D();
+        grid = new cell[20][13];
         startGrid();
+        clear = false;
         //mainGroup.getChildren().add(canvas);
         mainScene = new Scene(mainGroup);
+        mainGroup.requestFocus();
         mainGroup.setOnMousePressed(e -> {
-            System.out.println("pressed x:" + e.getX() + " y:" + e.getY());
-            updateGrid(e.getX(), e.getY());
+            switch (e.getButton()) {
+                case PRIMARY:
+                    System.out.println("pressed x:" + e.getX() + " y:" + e.getY());
+                    updateGrid(e.getX(), e.getY());
+                    break;
+                case SECONDARY:
+                    break;
+            }
         });
         mainGroup.setOnMouseDragged(e -> {
             System.out.println("dragged x:" + e.getX() + " y:" + e.getY());
             updateGrid(e.getX(), e.getY());
+        });
+        mainGroup.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case C:
+                    if(!clear)
+                        clear = true;
+                    else
+                        clear = false;
+                    break;
+                case X:
+                    startGrid();
+                    break;
+            }
         });
         //primaryStage.setResizable(false);
         primaryStage.setScene(mainScene);
@@ -62,7 +85,7 @@ public class Main extends Application {
     void startGrid() {
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[i].length; j++) {
-                grid[i][j] = new Rectangle(i * side, j * side, side, side);
+                grid[i][j] = new cell(i * side, j * side, side, side);
                 grid[i][j].setFill(Color.WHITE);
                 grid[i][j].setStroke(Color.BLACK);
                 mainGroup.getChildren().add(grid[i][j]);
@@ -73,10 +96,11 @@ public class Main extends Application {
     void updateGrid(double x, double y) {
         for(int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                double x1 = grid[i][j].getX();
-                double y1 = grid[i][j].getY();
                 if(x >= grid[i][j].getX() && y >= grid[i][j].getY() && x <= (grid[i][j].getX() + side) && y <= (grid[i][j].getY() + side)) {
-                    grid[i][j].setFill(Color.BLACK);
+                    if(!clear)
+                        grid[i][j].setFill(Color.BLACK);
+                    else
+                        grid[i][j].setFill(Color.WHITE);
                 }
             }
         }
